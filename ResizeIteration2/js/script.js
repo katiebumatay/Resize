cc823f7ac9d484d9978c6bbeb32e96c7f4c3db9b
Katie Bumatay <bumatay.katie@gmail.com>
2018-11-24 15:40:46 -0600

    iterations with combined effects

---
+++ ResizeIteration2/js/script.js
@@ -0,0 +1,400 @@
+
+
+// var int = 500;
+
+// function intervalTime() {
+// 	int = 100;
+// 	console.log(int);
+
+// }
+
+// setInterval('showBox()', 'makeInterval()');
+
+
+// function makeInterval() {
+
+// 	var int = 500;
+
+
+// 	if (w <= 400) {
+// 		if (h <= 200) {
+// 			int = 50; 
+// 		}
+// 		else if (h <= 320) {
+// 			int = 100;
+// 		}
+// 		else if (h <= 440) {
+// 			int = 150; 
+// 		}
+// 		else if (h <= 560) {
+// 			int = 200; 
+// 		}
+// 		else if (h <= 680) {
+// 			int = 250;
+// 		}
+// 		else {
+// 			int = 300; 
+// 		}
+// 	}
+
+// 	else if (w <= 600) {
+// 		if (h <= 200) {
+// 			int = 100;
+// 		}
+// 		else if (h <= 320) {
+// 			int = 150;
+// 		}
+// 		else if (h <= 440) {
+// 			int = 200;
+// 		}
+// 		else if (h <= 560) {
+// 			int = 250;
+// 		}
+// 		else if (h <= 680) {
+// 			int = 300;
+// 		}
+// 		else {
+// 			int = 350;
+// 		}
+// 	}
+
+// 	else if (w <= 800) {
+// 		if (h <= 200) {
+// 			int = 150; 
+// 		}
+// 		else if (h <= 320) {
+// 			int = 200; 
+// 		}
+// 		else if (h <= 440) {
+// 			int = 250; 
+// 		}
+// 		else if (h <= 560) {
+// 			int = 300; 
+// 		}
+// 		else if (h <= 680) {
+// 			int = 350; 
+// 		}
+// 		else {
+// 			int = 400; 
+// 		}
+// 	}
+
+// 	else if (w <= 1000) {
+// 		if (h <= 200) {
+// 			int = 200; 
+// 		}
+// 		else if (h <= 320) {
+// 			int = 250; 
+// 		}
+// 		else if (h <= 440) {
+// 			int = 300; 
+// 		}
+// 		else if (h <= 560) {
+// 			int = 350; 
+// 		}
+// 		else if (h <= 680) {
+// 			int = 400; 
+// 		}
+// 		else {
+// 			int = 450; 
+// 		}
+// 	}
+
+// 	else if (w <= 1200) {
+// 		if (h <= 200) {
+// 			int = 250; 
+// 		}
+// 		else if (h <= 320) {
+// 			int = 300; 
+// 		}
+// 		else if (h <= 440) {
+// 			int = 350; 
+// 		}
+// 		else if (h <= 560) {
+// 			int = 400; 
+// 		}
+// 		else if (h <= 680) {
+// 			int = 450; 
+// 		}
+// 		else {
+// 			int = 500; 
+// 		}
+// 	}
+
+// 	else {
+// 		if (h <= 200) {
+// 			int = 300; 
+// 		}
+// 		else if (h <= 320) {
+// 			int = 350; 
+// 		}
+// 		else if (h <= 440) {
+// 			int = 400; 
+// 		}
+// 		else if (h <= 560) {
+// 			int = 450; 
+// 		}
+// 		else if (h <= 680) {
+// 			int = 500; 
+// 		}
+// 		else {
+// 			int = 550;
+// 		}
+// 	}
+
+// 	return int;
+
+// };
+
+
+function showBox() {
+
+	// intervalTime();
+
+	// console.log("in showBox method");
+
+	var w = window.innerWidth;
+	var h = window.innerHeight;
+
+	var num; 
+
+	if (w <= 400) {
+		if (h <= 200) {
+			num = Math.floor(Math.random() * 1024) + 1; 
+			setTimeout(showBox, 50);
+		}
+		else if (h <= 320) {
+			num = Math.floor(Math.random() * 512) + 1;
+			setTimeout(showBox, 100);
+		}
+		else if (h <= 440) {
+			num = Math.floor(Math.random() * 256) + 1; 
+			setTimeout(showBox, 150);
+		}
+		else if (h <= 560) {
+			num = Math.floor(Math.random() * 128) + 1; 
+			setTimeout(showBox, 200);
+		}
+		else if (h <= 680) {
+			num = Math.floor(Math.random() * 64) + 1; 
+			setTimeout(showBox, 250);
+		}
+		else {
+			num = Math.floor(Math.random() * 32) + 1; 
+			setTimeout(showBox, 300);
+		}
+	}
+
+	else if (w <= 600) {
+		if (h <= 200) {
+			num = Math.floor(Math.random() * 512) + 1; 
+			setTimeout(showBox, 100);
+		}
+		else if (h <= 320) {
+			num = Math.floor(Math.random() * 256) + 1; 
+			setTimeout(showBox, 150);
+		}
+		else if (h <= 440) {
+			num = Math.floor(Math.random() * 128) + 1;
+			setTimeout(showBox, 200); 
+		}
+		else if (h <= 560) {
+			num = Math.floor(Math.random() * 64) + 1;
+			setTimeout(showBox, 250);
+		}
+		else if (h <= 680) {
+			num = Math.floor(Math.random() * 32) + 1; 
+			setTimeout(showBox, 300);
+		}
+		else {
+			num = Math.floor(Math.random() * 16) + 1; 
+			setTimeout(showBox, 350);
+		}
+	}
+
+	else if (w <= 800) {
+		if (h <= 200) {
+			num = Math.floor(Math.random() * 256) + 1; 
+			setTimeout(showBox, 150);
+		}
+		else if (h <= 320) {
+			num = Math.floor(Math.random() * 128) + 1; 
+			setTimeout(showBox, 200); 
+		}
+		else if (h <= 440) {
+			num = Math.floor(Math.random() * 64) + 1; 
+			setTimeout(showBox, 250); 
+		}
+		else if (h <= 560) {
+			num = Math.floor(Math.random() * 32) + 1; 
+			setTimeout(showBox, 300);
+		}
+		else if (h <= 680) {
+			num = Math.floor(Math.random() * 16) + 1; 
+			setTimeout(showBox, 350);
+		}
+		else {
+			num = Math.floor(Math.random() * 8) + 1;
+			setTimeout(showBox, 400); 
+		}
+	}
+
+	else if (w <= 1000) {
+		if (h <= 200) {
+			num = Math.floor(Math.random() * 128) + 1; 
+			setTimeout(showBox, 200); 
+		}
+		else if (h <= 320) {
+			num = Math.floor(Math.random() * 64) + 1;
+			setTimeout(showBox, 250); 
+		}
+		else if (h <= 440) {
+			num = Math.floor(Math.random() * 32) + 1; 
+			setTimeout(showBox, 300);
+		}
+		else if (h <= 560) {
+			num = Math.floor(Math.random() * 16) + 1; 
+			setTimeout(showBox, 350);
+		}
+		else if (h <= 680) {
+			num = Math.floor(Math.random() * 8) + 1;
+			setTimeout(showBox, 400);  
+		}
+		else {
+			num = Math.floor(Math.random() * 4) + 1; 
+			setTimeout(showBox, 450); 
+		}
+	}
+
+	else if (w <= 1200) {
+		if (h <= 200) {
+			num = Math.floor(Math.random() * 64) + 1; 
+			setTimeout(showBox, 250);
+		}
+		else if (h <= 320) {
+			num = Math.floor(Math.random() * 32) + 1; 
+			setTimeout(showBox, 300);
+		}
+		else if (h <= 440) {
+			num = Math.floor(Math.random() * 16) + 1; 
+			setTimeout(showBox, 350);
+
+		}
+		else if (h <= 560) {
+			num = Math.floor(Math.random() * 8) + 1;
+			setTimeout(showBox, 400);  
+		}
+		else if (h <= 680) {
+			num = Math.floor(Math.random() * 4) + 1; 
+			setTimeout(showBox, 450); 
+		}
+		else {
+			num = Math.floor(Math.random() * 2) + 1; 
+			setTimeout(showBox, 500); 
+		}
+	}
+
+	else {
+		if (h <= 200) {
+			num = Math.floor(Math.random() * 32) + 1; 
+			setTimeout(showBox, 300);
+		}
+		else if (h <= 320) {
+			num = Math.floor(Math.random() * 16) + 1;
+			setTimeout(showBox, 350); 
+		}
+		else if (h <= 440) {
+			num = Math.floor(Math.random() * 8) + 1; 
+			setTimeout(showBox, 400); 
+		}
+		else if (h <= 560) {
+			num = Math.floor(Math.random() * 4) + 1; 
+			setTimeout(showBox, 450); 
+		}
+		else if (h <= 680) {
+			num = Math.floor(Math.random() * 2) + 1; 
+			setTimeout(showBox, 500); 
+		}
+		else {
+			num = 1; 
+			setTimeout(showBox, 550); 
+		}
+	}
+
+	var showNum = Math.floor(Math.random() * 3) + 1;
+	var showClass = "show" + showNum
+	var theID = "#box" + num;
+
+	$(theID).toggleClass(showClass);
+
+	console.log(num);
+};
+
+
+
+
+
+$(document).ready(function() {
+
+	showBox();
+
+	// $.fn.showBox = function() {
+
+	// 	// var num = 1;
+
+	// 	// if ((".box").css(width) == "94vw") {
+	// 	// 	// var num = Math.floor(Math.random() * 2)
+	// 	// 	$("box:nth-child(num)").toggleClass("show");
+	// 	// }
+	// 	// if ((".box").css(width) == "46vw") {
+	// 	// 	var num = Math.floor(Math.random() * 4)
+	// 	// 	$("box:nth-child(num)").toggleClass("show");
+	// 	// }
+	// 	// else {
+	// 	// 	$("box:nth-child(num)").toggleClass("show");
+	// 	// }
+
+	// 	$(this).toggleClass("show");
+	// };
+
+	// setInterval('showBox()', 800);
+
+	// $.fn.switchSpots = function() {
+ //        var cells = $(".box");
+ //        for(var i = 0; i < cells.length; i++){
+ //            var target = Math.floor(Math.random() * cells.length -1) + 1;
+ //            var target2 = Math.floor(Math.random() * cells.length -1) +1;
+ //            cells.eq(target).before(cells.eq(target2));
+ //        }
+ //    }
+    
+
+ //     $(function(){
+
+ //        $('body').switchSpots();
+
+ //     });
+
+
+
+    $('.box').mouseenter(function(){
+    	// $(this).showBox();
+        $(this).toggleClass("expand");
+    });
+
+});
+
+// setInterval('makeDiv()', 800);
+
+
+// function makeDiv() {
+//     $("<div></div>").addClass("box").appendTo(".container");
+//     console.log("making div")
+// };
+
+
+
+
+
+
+
---
+++ ResizeIteration2/js/script2.js
@@ -0,0 +1,256 @@
+
+
+function showBox() {
+
+	// intervalTime();
+
+	// console.log("in showBox method");
+
+	var w = window.innerWidth;
+	var h = window.innerHeight;
+
+	var num; 
+
+	if (w <= 400) {
+		if (h <= 200) {
+			num = Math.floor(Math.random() * 1024) + 1; 
+			setTimeout(showBox, 50);
+		}
+		else if (h <= 320) {
+			num = Math.floor(Math.random() * 512) + 1;
+			setTimeout(showBox, 100);
+		}
+		else if (h <= 440) {
+			num = Math.floor(Math.random() * 256) + 1; 
+			setTimeout(showBox, 150);
+		}
+		else if (h <= 560) {
+			num = Math.floor(Math.random() * 128) + 1; 
+			setTimeout(showBox, 200);
+		}
+		else if (h <= 680) {
+			num = Math.floor(Math.random() * 64) + 1; 
+			setTimeout(showBox, 250);
+		}
+		else {
+			num = Math.floor(Math.random() * 32) + 1; 
+			setTimeout(showBox, 300);
+		}
+	}
+
+	else if (w <= 600) {
+		if (h <= 200) {
+			num = Math.floor(Math.random() * 512) + 1; 
+			setTimeout(showBox, 100);
+		}
+		else if (h <= 320) {
+			num = Math.floor(Math.random() * 256) + 1; 
+			setTimeout(showBox, 150);
+		}
+		else if (h <= 440) {
+			num = Math.floor(Math.random() * 128) + 1;
+			setTimeout(showBox, 200); 
+		}
+		else if (h <= 560) {
+			num = Math.floor(Math.random() * 64) + 1;
+			setTimeout(showBox, 250);
+		}
+		else if (h <= 680) {
+			num = Math.floor(Math.random() * 32) + 1; 
+			setTimeout(showBox, 300);
+		}
+		else {
+			num = Math.floor(Math.random() * 16) + 1; 
+			setTimeout(showBox, 350);
+		}
+	}
+
+	else if (w <= 800) {
+		if (h <= 200) {
+			num = Math.floor(Math.random() * 256) + 1; 
+			setTimeout(showBox, 150);
+		}
+		else if (h <= 320) {
+			num = Math.floor(Math.random() * 128) + 1; 
+			setTimeout(showBox, 200); 
+		}
+		else if (h <= 440) {
+			num = Math.floor(Math.random() * 64) + 1; 
+			setTimeout(showBox, 250); 
+		}
+		else if (h <= 560) {
+			num = Math.floor(Math.random() * 32) + 1; 
+			setTimeout(showBox, 300);
+		}
+		else if (h <= 680) {
+			num = Math.floor(Math.random() * 16) + 1; 
+			setTimeout(showBox, 350);
+		}
+		else {
+			num = Math.floor(Math.random() * 8) + 1;
+			setTimeout(showBox, 400); 
+		}
+	}
+
+	else if (w <= 1000) {
+		if (h <= 200) {
+			num = Math.floor(Math.random() * 128) + 1; 
+			setTimeout(showBox, 200); 
+		}
+		else if (h <= 320) {
+			num = Math.floor(Math.random() * 64) + 1;
+			setTimeout(showBox, 250); 
+		}
+		else if (h <= 440) {
+			num = Math.floor(Math.random() * 32) + 1; 
+			setTimeout(showBox, 300);
+		}
+		else if (h <= 560) {
+			num = Math.floor(Math.random() * 16) + 1; 
+			setTimeout(showBox, 350);
+		}
+		else if (h <= 680) {
+			num = Math.floor(Math.random() * 8) + 1;
+			setTimeout(showBox, 400);  
+		}
+		else {
+			num = Math.floor(Math.random() * 4) + 1; 
+			setTimeout(showBox, 450); 
+		}
+	}
+
+	else if (w <= 1200) {
+		if (h <= 200) {
+			num = Math.floor(Math.random() * 64) + 1; 
+			setTimeout(showBox, 250);
+		}
+		else if (h <= 320) {
+			num = Math.floor(Math.random() * 32) + 1; 
+			setTimeout(showBox, 300);
+		}
+		else if (h <= 440) {
+			num = Math.floor(Math.random() * 16) + 1; 
+			setTimeout(showBox, 350);
+
+		}
+		else if (h <= 560) {
+			num = Math.floor(Math.random() * 8) + 1;
+			setTimeout(showBox, 400);  
+		}
+		else if (h <= 680) {
+			num = Math.floor(Math.random() * 4) + 1; 
+			setTimeout(showBox, 450); 
+		}
+		else {
+			num = Math.floor(Math.random() * 2) + 1; 
+			setTimeout(showBox, 500); 
+		}
+	}
+
+	else {
+		if (h <= 200) {
+			num = Math.floor(Math.random() * 32) + 1; 
+			setTimeout(showBox, 300);
+		}
+		else if (h <= 320) {
+			num = Math.floor(Math.random() * 16) + 1;
+			setTimeout(showBox, 350); 
+		}
+		else if (h <= 440) {
+			num = Math.floor(Math.random() * 8) + 1; 
+			setTimeout(showBox, 400); 
+		}
+		else if (h <= 560) {
+			num = Math.floor(Math.random() * 4) + 1; 
+			setTimeout(showBox, 450); 
+		}
+		else if (h <= 680) {
+			num = Math.floor(Math.random() * 2) + 1; 
+			setTimeout(showBox, 500); 
+		}
+		else {
+			num = 1; 
+			setTimeout(showBox, 550); 
+		}
+	}
+
+	var showNum = Math.floor(Math.random() * 3) + 1;
+	var showClass = "show" + showNum
+	var theID = "#box" + num;
+
+	$(theID).toggleClass(showClass);
+
+	console.log(num);
+};
+
+
+
+
+
+$(document).ready(function() {
+
+	showBox();
+
+	// $.fn.showBox = function() {
+
+	// 	// var num = 1;
+
+	// 	// if ((".box").css(width) == "94vw") {
+	// 	// 	// var num = Math.floor(Math.random() * 2)
+	// 	// 	$("box:nth-child(num)").toggleClass("show");
+	// 	// }
+	// 	// if ((".box").css(width) == "46vw") {
+	// 	// 	var num = Math.floor(Math.random() * 4)
+	// 	// 	$("box:nth-child(num)").toggleClass("show");
+	// 	// }
+	// 	// else {
+	// 	// 	$("box:nth-child(num)").toggleClass("show");
+	// 	// }
+
+	// 	$(this).toggleClass("show");
+	// };
+
+	// setInterval('showBox()', 800);
+
+	// $.fn.switchSpots = function() {
+ //        var cells = $(".box");
+ //        for(var i = 0; i < cells.length; i++){
+ //            var target = Math.floor(Math.random() * cells.length -1) + 1;
+ //            var target2 = Math.floor(Math.random() * cells.length -1) +1;
+ //            cells.eq(target).before(cells.eq(target2));
+ //        }
+ //    }
+    
+
+ //     $(function(){
+
+ //        $('body').switchSpots();
+
+ //     });
+
+
+
+    $('.box').mouseenter(function(){
+    	var effect = Math.floor(Math.random() * 8) + 1;
+    	var effectClass = "effect" + effect
+    	// $(this).showBox();
+        $(this).toggleClass(effectClass);
+        console.log(effectClass);
+    });
+
+});
+
+// setInterval('makeDiv()', 800);
+
+
+// function makeDiv() {
+//     $("<div></div>").addClass("box").appendTo(".container");
+//     console.log("making div")
+// };
+
+
+
+
+
+
+
