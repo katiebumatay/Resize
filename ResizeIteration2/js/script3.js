32110351b049cf9ff22ffee574b9173c900d4473
Katie Bumatay <bumatay.katie@gmail.com>
2018-11-24 16:10:40 -0600

    delete comments

--- ResizeIteration2/js/script3.js
+++ ResizeIteration2/js/script3.js
@@ -191,42 +191,7 @@ $(document).ready(function() {
 
 	showBox();
 
-	// $.fn.showBox = function() {
 
-	// 	// var num = 1;
-
-	// 	// if ((".box").css(width) == "94vw") {
-	// 	// 	// var num = Math.floor(Math.random() * 2)
-	// 	// 	$("box:nth-child(num)").toggleClass("show");
-	// 	// }
-	// 	// if ((".box").css(width) == "46vw") {
-	// 	// 	var num = Math.floor(Math.random() * 4)
-	// 	// 	$("box:nth-child(num)").toggleClass("show");
-	// 	// }
-	// 	// else {
-	// 	// 	$("box:nth-child(num)").toggleClass("show");
-	// 	// }
-
-	// 	$(this).toggleClass("show");
-	// };
-
-	// setInterval('showBox()', 800);
-
-	// $.fn.switchSpots = function() {
- //        var cells = $(".box");
- //        for(var i = 0; i < cells.length; i++){
- //            var target = Math.floor(Math.random() * cells.length -1) + 1;
- //            var target2 = Math.floor(Math.random() * cells.length -1) +1;
- //            cells.eq(target).before(cells.eq(target2));
- //        }
- //    }
-    
-
- //     $(function(){
-
- //        $('body').switchSpots();
-
- //     });
  	var effect;
  	var effectClass;
 
@@ -240,14 +205,6 @@ $(document).ready(function() {
     });
 
 });
-
-// setInterval('makeDiv()', 800);
-
-
-// function makeDiv() {
-//     $("<div></div>").addClass("box").appendTo(".container");
-//     console.log("making div")
-// };
 
 
 
